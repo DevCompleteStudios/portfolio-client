c6d11ee99531f558336570c40123bce532916113
DevCompleteStudios <jorge.yael333@gmail.com>
2024-10-28 08:59:48 -0600

    Agregando comentarios - falta paginacion y traerlos desde el servidor

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IComent } from './interfaces/IComent';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,30 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  protected coments: IComent[] = [
+    {
+      content: 'Yo he comprado varios de sus servicios y son de la mas alta calidad!',
+      date: new Date(),
+      id: 1,
+      stars: 5,
+      username: 'Panchito Perez Gonzales',
+    },
+    {
+      content: 'Yo he comprado varios de sus servicios y son de la mas alta calidad!',
+      date: new Date(),
+      id: 1,
+      stars: 3,
+      username: 'Panchito Perez Gonzales',
+    },
+  ];
+
+
+  get getStars():number[]{
+    return Array(5).fill(0);
+  }
+
+  checkStar(coment: IComent, index: number): boolean {
+    return index < coment.stars;
+  }
 
 }
---
+++ src/app/pages/home/interfaces/IComent.ts
@@ -0,0 +1,8 @@
+
+export interface IComent {
+  content: string;
+  username: string;
+  stars: number;
+  date: Date;
+  id: number;
+}
