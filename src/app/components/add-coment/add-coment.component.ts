f6d9181931f93d2c8719e750e738d78ca14929bf
DevCompleteStudios <jorge.yael333@gmail.com>
2024-10-28 20:16:25 -0600

    Iniciando servicios

---
+++ src/app/components/add-coment/add-coment.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-coment',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-coment.component.html',
+})
+export class AddComentComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { AboutMeComponent } from "../../components/about-me/about-me.component";
 import { ContactMeComponent } from "../../components/contact-me/contact-me.component";
 import { ViewComentsComponent } from "../../components/view-coments/view-coments.component";
+import { AddComentComponent } from "../../components/add-coment/add-coment.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [AboutMeComponent, ContactMeComponent, ViewComentsComponent],
+  imports: [AboutMeComponent, ContactMeComponent, ViewComentsComponent, AddComentComponent],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
