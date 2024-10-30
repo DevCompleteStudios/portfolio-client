03ff440d74a16b5526c51da7053e2d90762753f0
DevCompleteStudios <jorge.yael333@gmail.com>
2024-10-30 07:32:46 -0600

    navbar agregado

---
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-nav-bar',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './nav-bar.component.html',
+})
+export class NavBarComponent implements OnInit {
+
+  constructor(private activatedRoute: ActivatedRoute){}
+
+  ngOnInit() {
+    this.activatedRoute.fragment.subscribe((fragment: string | null) => {
+      if (fragment) this.jumpToSection(fragment);
+    });
+  }
+
+  jumpToSection(section: string | null) {
+    if (section) document.getElementById(section)?.scrollIntoView({ behavior: 'smooth' });
+  }
+
+}
